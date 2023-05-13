435d9f1cce8ea20219713069c93690c4eefeaf18
IrynaBuchma <buchma.iryna2018@gmail.com>
2023-05-14 00:31:17 +0300

    Update App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,81 +1,69 @@
 import React, { Component } from 'react';
-// import Modal from '../components/Modal/Modal';
 import Button from '../components/Button/Button';
-// import {ReactComponent as SearchIcon} from './icons/search.svg';
 import Searchbar from './Searchbar/Searchbar';
-import { fetchImages } from './FetchImages/FetchImages';
+import { fetchImages } from '../service/FetchImages';
 import ImageGallery from './ImageGallery/ImageGallery';
 import Loader from './Loader/Loader';
 import Notiflix from 'notiflix';
 
 export class App extends Component {
-
+  
   state = {
     inputData: '',
     images:[],
-    status: 'idle',
     totalHits: 0,
-    page: 1
+    page: 1,
+    status: idle,
+    isButtonShown: false,
   }
+  
+  componentDidUpdate(prevProps, prevState) {
 
-  componentDidUpdate(prevState) {
-    if (this.state.inputData !== prevState.inputData) {
-      this.onNextPage(this.state.inputData);
+    const newInput = this.state.inputData;
+    const newPage = this.state.page;
+
+    if (newInput !== prevProps.inputData || newPage !== prevState.page) {
+      this.fetchData(newInput, newPage);
     }
-    return;
   }
 
-  fetchData = async (inputData) => {
+  handleFormsubmit = (newInput) => {
+
+      this.setState({ 
+        inputData: newInput,
+      });
+    }
+
+    fetchData = async(newInput, newPage) => {
+      
       try {
         this.setState({ status: 'pending'});
-        const { totalHits, hits, page } = await fetchImages(inputData);
+        const { totalHits, hits } = await fetchImages(newInput, newPage);
           if(hits.length < 1) {
           this.setState({ status: 'idle'});
           Notiflix.Notify.info('Sorry, there are no images matching your search query');
+          return;
           }
-        else {
-          this.setState({
-            images: hits,
-            inputData,
-            totalHits: totalHits,
-            status: 'resolved',
-            page,
-          })
-        }
-      }
+              this.setState(prevState => ({
+              images: [...prevState.images, ...hits],
+              totalHits: totalHits,
+              isButtonShown: true,
+              status: 'resolved',
+              }))
+          }
+      
       catch (error) {
         this.setState({ status: 'rejected' });
       }
-    }    
+    }  
 
-  handleFormsubmit = async (inputData) => {
-
-    if(inputData.trim() === '') {
-      Notiflix.Notify.failure('Please input request data');
-      return;
-    } 
-    else {
-      this.fetchData(inputData);
-    }
+  onNextPage = () => {
+    this.setState(prevState => ({
+      page: prevState.page + 1,
+      isButtonShown: true,
+    }))
   }
-
-  onNextPage = async() => {
-    this.setState({ status: 'pending' });
-
-    try {
-      const { inputData, page } = this.state;
-      const { hits } = await fetchImages(inputData, page + 1);
-      this.setState(prevState => ({
-        page: prevState.page + 1,
-        images: [...prevState.images, ...hits],
-        status: 'resolved'
-      }))
-
-    } catch (error) {
-      this.setState({ status: 'rejected' });
-    }
-  }
-
+    
   render() {
 
     const { totalHits, status, images, page } = this.state;
